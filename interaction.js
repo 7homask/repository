dd217e15f3255abdaef330749cc63f45cd0bb4d5
7homask <thomas.keeble@florey.edu.au>
2016-02-01 14:39:47 +1100

    making the cat purr

--- interaction.js
+++ interaction.js
@@ -1,1 +1,24 @@
-console.log('hello world')
+// interactive recipe
+
+// get HTML elements
+
+var cat_img = document.getElementById("cat_img");
+
+
+// add an event listener (ie. do something on click)
+
+cat_img.addEventListener("click", meow);
+
+function meow(){
+	alert("Meow");
+} 
+
+var button = document.getElementById("button");
+
+button.addEventListener("click", purr);
+function purr(){
+	alert("Purr")
+}
+
+
+// manipulate HTML element
